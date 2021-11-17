de763b9cc0a976b90858703ba55862bc1e136c44
Amiel Martin <amiel.martin@gmail.com>
2021-11-17 08:42:12 -0900

    Spike communicates between rust and neovim

---
+++ plugin/neovim-tmux-navigator.vim
@@ -0,0 +1,49 @@
+" Initialize the channel
+if !exists('s:jobId')
+	let s:jobId = 0
+endif
+
+" Constants for RPC messages.
+let s:Up = 'up'
+let s:Down = 'down'
+let s:Left = 'left'
+let s:Right = 'right'
+
+" The path to the binary that was created out of 'cargo build' or 'cargo build --release". This will generally be 'target/release/name'
+let s:bin = '/Users/amiel/src/neovim-tmux-navigator/target/debug/neovim-tmux-navigator'
+
+" Entry point. Initialize RPC. If it succeeds, then attach commands to the `rpcnotify` invocations.
+function! s:connect()
+  let id = s:initRpc()
+  
+  if 0 == id
+    echoerr "neovim-tmux-navigator: cannot start rpc process"
+  elseif -1 == id
+    echoerr "neovim-tmux-navigator: rpc process is not executable"
+  else
+    " Mutate our s:jobId variable to hold the channel ID
+    let s:jobId = id 
+    
+    call s:configureCommands()
+  endif
+endfunction
+
+function! s:configureCommands()
+  command! NvimTmuxNavigatorUp :call rpcnotify(s:jobId, s:Up)
+  command! NvimTmuxNavigatorDown :call rpcnotify(s:jobId, s:Down)
+  command! NvimTmuxNavigatorLeft :call rpcnotify(s:jobId, s:Left)
+  command! NvimTmuxNavigatorRight :call rpcnotify(s:jobId, s:Right)
+endfunction
+
+" Initialize RPC
+function! s:initRpc()
+  if s:jobId == 0
+    let jobid = jobstart([s:bin], { 'rpc': v:true })
+    return jobid
+  else
+    echo "neovim-tmux-navigator: already started"
+    return s:jobId
+  endif
+endfunction
+
+call s:connect()
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,75 @@
+use neovim_lib::{Neovim, NeovimApi, Session};
+
+enum Message {
+    Up,
+    Down,
+    Left,
+    Right,
+    Unknown(String),
+}
+
+impl From<String> for Message {
+    fn from(event: String) -> Self {
+        match &event[..] {
+            "up" => Message::Up,
+            "down" => Message::Down,
+            "left" => Message::Left,
+            "right" => Message::Right,
+            _ => Message::Unknown(event),
+        }
+    }
+}
+
+struct EventHandler {
+    nvim: Neovim,
+}
+
+impl EventHandler {
+    fn new() -> EventHandler {
+        let session = Session::new_parent().unwrap();
+        let nvim = Neovim::new(session);
+
+        EventHandler { nvim }
+    }
+
+    fn recv(&mut self) {
+        let receiver = self.nvim.session.start_event_loop_channel();
+
+        for (event, _values) in receiver {
+            match Message::from(event) {
+                Message::Up => {
+                    // let nums = values
+                    //     .iter()
+                    //     .map(|v| v.as_i64().unwrap())
+                    //     .collect::<Vec<i64>>();
+
+                    // let sum = self.calculator.add(nums);
+                    // self.nvim
+                    //     .command(&format!("echo \"Sum: {}\"", sum.to_string()))
+                    //     .unwrap();
+                }
+
+                Message::Down => {
+                    self.nvim
+                        .command(&format!("echo \"Product: {}\"", "Down"))
+                        .unwrap();
+                }
+                Message::Left => {}
+                Message::Right => {}
+
+                // Handle anything else
+                Message::Unknown(event) => {
+                    self.nvim
+                        .command(&format!("echo \"Unknown command: {}\"", event))
+                        .unwrap();
+                }
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut event_handler = EventHandler::new();
+
+    event_handler.recv();
 }
