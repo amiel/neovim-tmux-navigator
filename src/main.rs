1ce1cf9a30771c2f77af948aff13e99656d32d16
Amiel Martin <amiel.martin@gmail.com>
2022-04-13 13:07:40 -0800

    Log panics to a file for debugging

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,20 @@ fn main() {
     let args = cli::build_cli().get_matches();
 
     match args.subcommand() {
-        ("server", Some(sub_args)) => neovim_server::EventHandler::new(sub_args).recv(),
-        ("client", Some(sub_args)) => tmux_client::Handler::new(sub_args).call(),
+        ("server", Some(sub_args)) => {
+            simple_logging::log_to_file("neovim-tmux-navigator-server.log", log::LevelFilter::Info)
+                .unwrap();
+            log_panics::init();
+
+            neovim_server::EventHandler::new(sub_args).recv()
+        }
+        ("client", Some(sub_args)) => {
+            simple_logging::log_to_file("neovim-tmux-navigator-client.log", log::LevelFilter::Info)
+                .unwrap();
+            log_panics::init();
+
+            tmux_client::Handler::new(sub_args).call()
+        }
         _ => {}
     }
 }
